7078b1a30b1c14e8415eceec23907f1be0ed90d4
idoceb00 <idoceb00@estudiantes.unileon.es>
2024-04-16 11:39:43 +0200

    add: desarrollo de la función que calcula los vectores de posiciones de cadenas, en base al alfabeto

--- vigenere.py
+++ vigenere.py
@@ -1,7 +1,8 @@
-# FLUJO PRINCIPAL DEL PROGRAMA
+import numpy as np
 
-def extiende_clave(clave, long_msj):
-    # Crea la clave extendida en base a la longitud del mensaje
+
+def extiende_clave_ciclicamente(clave_num, long_msj):
+    # Crea la clave extendida en base a la longitud del mensaje, ciclicamenete
 
     stop = False
     clave_extendida = ""
@@ -9,15 +10,29 @@ def extiende_clave(clave, long_msj):
     while not stop:
         resto = long_msj - clave_extendida.length
 
-        if clave.length == long_msj:
+        if clave_num.length == long_msj:
             stop = True
-        elif resto < clave.length:
-            clave_extendida += clave[0:resto - 1]
+        elif resto < clave_num.length:
+            clave_extendida += clave_num[0:resto - 1]
         else:
-            clave_extendida += clave
+            clave_extendida += clave_num
 
     return clave_extendida
 
 
+def genera_vector(cadena, alfabeto):
+    # Crea el vector númerico en base a las posiciones en el alfabeto de de cada uno de los caracteres de la cadena
+    lista = list(alfabeto)
+    vector = []
+
+    for c in cadena:
+        if c in lista:
+            vector.append(lista.index(c))
+
+    return vector
+
+
+# FLUJO PRINCIPAL DEL PROGRAMA
+genera_vector("abecemon", "abcdefghijklmno")
 
 
