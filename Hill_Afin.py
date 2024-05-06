ac85ac5b8b78dd0bb10af16adbc56610fa1c5650
acarbf01 <acarbf01@estudiantes.unileon.es>
2024-05-06 20:51:42 +0200

    Metodos finalizados v.1

--- Hill_Afin.py
+++ Hill_Afin.py
@@ -26,10 +26,36 @@ def genera_vector(cadena):
 # SEPARAR EN BLOQUES EL MENSAJE
 
 # MULTIPLICAR CADA BLOQUE POR LA MATRIZ INVERSA USANDO EL MÉTODO DE HILL
+def multiplica_bloques(mensaje_bloques, matriz_cifrado):
+    """
+        Realiza el producto del bloque asignado con la inversa de la matriz de cifrado.
+
+        Parámetros:
+        mensaje_bloques (Array): Array de vectores que contiene los bloques en los que se ha dividido el mensaje.
+        matriz_cifrado (Array): Matriz que sirve como clave de cifrado por la cual se multiplicara por su inversa.
+
+        Returns:
+        Array: Array con los resultados de los prouctos matriciales.
+    """
+    productos_bloques = np.array([])
+    
+    for bloque in mensaje_bloques:
+        result = np.array(bloque).dot(np.linalg.inv(matriz_cifrado))
+        productos_bloques = np.append(productos_bloques, result, axis=0)
+
+    return productos_bloques
 
 # CALCULO MODULAR
 
 # CONCATENAR BLOQUES
+def concatena_bloques(conjunto_bloques):
+    bloques_concatenados = []
+    
+    for bloque in conjunto_bloques:
+        bloques_concatenados = np.append(bloques_concatenados, bloque)
+
+    return bloques_concatenados
+
 
 # GENERAR EL MENSAJE
 
